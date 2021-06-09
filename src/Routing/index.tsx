22557d373be74f2203a50957268b08673079585a
Laptop-080 <laptop-080@Laptop-080s-MacBook-Pro.local>
2021-06-09 20:35:15 +0800

    update routing component

--- src/Routing/index.tsx
+++ src/Routing/index.tsx
@@ -1,36 +1,29 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  RouteComponentProps,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { RouteComponentProps, Switch, Route } from "react-router-dom";
 import routes from "../config/routes";
 interface RouteProps {}
 
 export const Routing: React.FC<RouteProps> = () => {
   return (
     <>
-      <Router>
-        <Switch>
-          {routes.map((route, index) => {
-            return (
-              <Route
-                key={index}
-                path={route.path}
-                exact={route.exact}
-                render={(props: RouteComponentProps<any>) => (
-                  <route.component
-                    name={route.name}
-                    {...props}
-                    {...route.props}
-                  />
-                )}
-              />
-            );
-          })}
-        </Switch>
-      </Router>
+      <Switch>
+        {routes.map((route, index) => {
+          return (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              render={(props: RouteComponentProps<any>) => (
+                <route.component
+                  name={route.name}
+                  {...props}
+                  {...route.props}
+                />
+              )}
+            />
+          );
+        })}
+      </Switch>
     </>
   );
 };
